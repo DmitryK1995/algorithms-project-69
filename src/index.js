96e057d8793b14f4a0b88bca8a391f34d48ff0da
‘DmitryK1995’ <KolesnikovD-S@yandex.ru>
2024-01-03 02:42:26 +0300

    fix TF-IDF formula

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-export function metricCalculate(text, searchWord, arr, documentsLength) {
+export function metricCalculate(text, searchWord, arr, docsCount) {
 	const splittedText = text.split(" ");
 
 	const countOfWords = splittedText.length;
-	const lengthOfOccuringDocuments = arr[searchWord].length;
+	const termCount = arr[searchWord].length;
 
 	const wordRepeated = splittedText.reduce(
 		(acc, word) => (word.replace(/[^a-zA-Z]+/g, "") == searchWord.replace(/[^a-zA-Z]+/g, "") ? acc + 1 : acc),
 		0,
 	);
-
-	return (wordRepeated / countOfWords) * Math.log10(documentsLength / lengthOfOccuringDocuments);
+	return (wordRepeated / countOfWords) * Math.log2(1 + (docsCount - termCount + 1) / (termCount + 0.5));
 }
 
 const searchEngine = (arrOfDocuments, searchString) => {
--- src/search.test.js
+++ src/search.test.js
@@ -64,3 +64,27 @@ test("search inputSentence", () => {
 
 	expect(search(docs, "shoot at me")).toEqual(["doc2", "doc3", "doc1"]);
 });
+
+test("search hexlet", () => {
+	const doc1 = { id: "doc1", text: "I can't shoot straight unless I've had a pint!" };
+	const doc2 = { id: "doc2", text: "Don't shoot shoot shoot that thing at me." };
+	const doc3 = { id: "doc3", text: "Don't shoot shoot shoot shoor that thing at me." };
+	const doc4 = { id: "doc4", text: "I'm your shooter." };
+
+	const docs = [doc1, doc2, doc3, doc4];
+
+	expect(search(docs, "shoot at me")).toEqual(["doc2", "doc3", "doc1"]);
+});
+
+test("short strings", async () => {
+	const doc1 = "I can't shoot straight unless I've had a pint!";
+	const doc2 = "Don't shoot shoot shoot that thing at me.";
+	const doc3 = "I'm your shooter.";
+	const docs = [
+		{ id: "doc1", text: doc1 },
+		{ id: "doc2", text: doc2 },
+		{ id: "doc3", text: doc3 },
+	];
+
+	expect(search(docs, "shoot at me, nerd")).toEqual(["doc2", "doc1"]);
+});
