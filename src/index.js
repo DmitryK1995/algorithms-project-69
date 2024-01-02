0058f29a4e39ed01f545df1812ff1fda389ef2d7
‘DmitryK1995’ <KolesnikovD-S@yandex.ru>
2024-01-03 02:04:03 +0300

    fix imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import { metricCalculate } from "./helpers/search-engine-helpers";
+export function metricCalculate(text, searchWord, arr, documentsLength) {
+	const splittedText = text.split(" ");
+
+	const countOfWords = splittedText.length;
+	const lengthOfOccuringDocuments = arr[searchWord].length;
+
+	const wordRepeated = splittedText.reduce(
+		(acc, word) => (word.replace(/[^a-zA-Z]+/g, "") == searchWord.replace(/[^a-zA-Z]+/g, "") ? acc + 1 : acc),
+		0,
+	);
+
+	return (wordRepeated / countOfWords) * Math.log10(documentsLength / lengthOfOccuringDocuments);
+}
 
 const searchEngine = (arrOfDocuments, searchString) => {
 	const arrayOfSearchWords = searchString.split(" ");
