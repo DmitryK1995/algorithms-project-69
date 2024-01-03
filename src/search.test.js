2cda63c07302cfa0c67f9961deb161e810e284a1
‘DmitryK1995’ <KolesnikovD-S@yandex.ru>
2024-01-03 03:08:53 +0300

    fix

--- src/search.test.js
+++ src/search.test.js
@@ -21,7 +21,7 @@ test("search punctuation", () => {
 });
 
 test("search empty docs list", () => {
-	expect(search([], "random")).toEqual([]);
+	expect(search([], "")).toEqual([]);
 });
 
 test("search without punctuation", () => {
@@ -32,6 +32,16 @@ test("search without punctuation", () => {
 	const docs = [doc1, doc2, doc3];
 
 	expect(search(docs, "pint")).toEqual(["doc1"]);
+});
+
+test("search with dot", () => {
+	const doc1 = { id: "doc1", text: "I can't shoot straight unless I've had a pint!" };
+	const doc2 = { id: "doc2", text: "Don't shoot shoot shoot that thing at me." };
+	const doc3 = { id: "doc3", text: "I'm your shooter." };
+
+	const docs = [doc1, doc2, doc3];
+
+	expect(search(docs, "shooter")).toEqual(["doc3"]);
 });
 
 test("search with punctuation", () => {
