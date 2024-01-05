f76368eb353d22998ccb2f92950ff775bc31ba3f
‘DmitryK1995’ <KolesnikovD-S@yandex.ru>
2024-01-05 22:58:05 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,85 +1,95 @@
 export function metricCalculate(text, searchWord, arr, docsCount) {
-	const splittedText = text.match(/\w+/g);
+  const splittedText = text.match(/\w+/g);
 
-	const termCount = arr[searchWord].length;
+  const termCount = arr[searchWord].length;
 
-	const wordRepeated = splittedText.reduce((acc, word) => (word === searchWord ? acc + 1 : acc), 0);
+  const wordRepeated = splittedText.reduce(
+    (acc, word) => (word === searchWord ? acc + 1 : acc),
+    0,
+  );
 
-	return wordRepeated * Math.log2(1 + (docsCount - termCount + 1) / (termCount + 0.5));
+  return (
+    wordRepeated *
+    Math.log2(1 + (docsCount - termCount + 1) / (termCount + 0.5))
+  );
 }
 
 const searchEngine = (arrOfDocuments, searchString) => {
-	const arrayOfSearchWords = searchString.match(/\w+/g);
+  const arrayOfSearchWords = searchString.match(/\w+/g);
 
-	if (!arrayOfSearchWords) return [];
+  if (!arrayOfSearchWords) return [];
 
-	const index = arrOfDocuments.reduce((acc, document) => {
-		const arrayOfWords = document.text.split(" ");
+  const index = arrOfDocuments.reduce((acc, document) => {
+    const arrayOfWords = document.text.split(" ");
 
-		arrayOfWords.forEach(word => {
-			const currentWordOnlyLetter = word.match(/\w+/g);
+    arrayOfWords.forEach((word) => {
+      const currentWordOnlyLetter = word.match(/\w+/g);
 
-			if (!currentWordOnlyLetter) return;
+      if (!currentWordOnlyLetter) return;
 
-			const currentWordOnlyLetters = currentWordOnlyLetter.join("");
+      const currentWordOnlyLetters = currentWordOnlyLetter.join("");
 
-			if (!acc[currentWordOnlyLetters]) {
-				acc[currentWordOnlyLetters] = [].concat(document.id);
-			} else {
-				acc[currentWordOnlyLetters].includes(document.id) ? acc : acc[currentWordOnlyLetters].push(document.id);
-			}
-		});
-		return acc;
-	}, {});
+      if (!acc[currentWordOnlyLetters]) {
+        acc[currentWordOnlyLetters] = [].concat(document.id);
+      } else {
+        acc[currentWordOnlyLetters].includes(document.id)
+          ? acc
+          : acc[currentWordOnlyLetters].push(document.id);
+      }
+    });
+    return acc;
+  }, {});
 
-	const arrayOfFound = arrayOfSearchWords.reduce((acc, word) => {
-		const searchWord = word;
+  const arrayOfFound = arrayOfSearchWords.reduce((acc, word) => {
+    const searchWord = word;
 
-		if (index[searchWord])
-			index[searchWord].forEach(docId => {
-				if (!acc[docId]) {
-					acc[docId] = 1;
-				} else {
-					acc[docId] += 1;
-				}
-			});
+    if (index[searchWord])
+      index[searchWord].forEach((docId) => {
+        if (!acc[docId]) {
+          acc[docId] = 1;
+        } else {
+          acc[docId] += 1;
+        }
+      });
 
-		return acc;
-	}, {});
+    return acc;
+  }, {});
 
-	const sortedDocsByRelevance = Object.entries(arrayOfFound).sort(([docName1, a], [docName2, b]) => {
-		if (a === b) {
-			const relevanceCoefA = arrayOfSearchWords.reduce(
-				(acc, word) =>
-					acc +
-					metricCalculate(
-						arrOfDocuments.find(el => el.id == docName1).text,
-						word,
-						index,
-						arrOfDocuments.length,
-					),
-				0,
-			);
+  const sortedDocsByRelevance = Object.entries(arrayOfFound).sort(
+    ([docName1, a], [docName2, b]) => {
+      if (a === b) {
+        const relevanceCoefA = arrayOfSearchWords.reduce(
+          (acc, word) =>
+            acc +
+            metricCalculate(
+              arrOfDocuments.find((el) => el.id == docName1).text,
+              word,
+              index,
+              arrOfDocuments.length,
+            ),
+          0,
+        );
 
-			const relevanceCoefB = arrayOfSearchWords.reduce(
-				(acc, word) =>
-					acc +
-					metricCalculate(
-						arrOfDocuments.find(el => el.id == docName2).text,
-						word,
-						index,
-						arrOfDocuments.length,
-					),
-				0,
-			);
+        const relevanceCoefB = arrayOfSearchWords.reduce(
+          (acc, word) =>
+            acc +
+            metricCalculate(
+              arrOfDocuments.find((el) => el.id == docName2).text,
+              word,
+              index,
+              arrOfDocuments.length,
+            ),
+          0,
+        );
 
-			return relevanceCoefB - relevanceCoefA;
-		}
+        return relevanceCoefB - relevanceCoefA;
+      }
 
-		return b - a;
-	});
+      return b - a;
+    },
+  );
 
-	return sortedDocsByRelevance.map(([id]) => id);
+  return sortedDocsByRelevance.map(([id]) => id);
 };
 
 export default searchEngine;
